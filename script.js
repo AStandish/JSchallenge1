a4b19df418d234ade64166fdbf45d30f5bd5a2f7
AStandish <112442942+AStandish@users.noreply.github.com>
2022-10-09 21:46:48 -0400

    moved code to generate password and created random character functionality

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -12,3 +12,28 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  const chars = "01234567890abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let passwordLength = prompt("Please insert a length greater than 8 and less than 128");
+  const password = "";
+
+  const minValue = 8
+  const maxValue = 128
+
+  console.log(passwordLength);
+
+  if (passwordLength <= 8 || passwordLength >= 128) {
+    console.log("wrong password")
+    return "wrong length"
+  }
+  const num = Math.round(Math.random () *chars.length-1)
+  const char = chars[num]
+  console.log(char)
+
+  for (i = 0; i <= passwordLength; i++) {
+   
+  }
+
+
+  return "hello"//password
+}
